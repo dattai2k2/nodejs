d2122269210b031e5e6c5e4e5968dd0cb4ddbae2
dattai2k2 <dattai2k2@gmai.com>
2023-07-12 22:38:45 +0700

    update project

---
+++ controller/controllerCategory.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const util = require('util')
+const mysql = require('mysql')
+const db = require('../db')
+
+module.exports = {
+    get: (req, res) => {
+        let sql = 'SELECT * FROM tbl_category'
+        db.query(sql, (err, response) => {
+            if (err) throw err
+            res.json(response)
+        })
+    },
+    detail: (req, res) => {
+        let sql = 'SELECT * FROM tbl_category WHERE id = ?'
+        db.query(sql, [req.params.id], (err, response) => {
+            if (err) throw err
+            res.json(response[0])
+        })
+    },
+    update: (req, res) => {
+        let data = req.body;
+        let productId = req.params.productId;
+        let sql = 'UPDATE tbl_category SET ? WHERE id = ?'
+        db.query(sql, [data, id], (err, response) => {
+            if (err) throw err
+            res.json({message: 'Update success!'})
+        })
+    },
+    new: (req, res) => {
+        let data = req.body;
+        let sql = 'INSERT INTO tbl_category SET ?'
+        db.query(sql, [data], (err, response) => {
+            if (err) throw err
+            res.json({message: 'Insert success!'})
+        })
+    },
+    delete: (req, res) => {
+        let id =req.params.id;
+        console.log(id);
+        let sql = 'DELETE FROM tbl_category WHERE id = ?'
+        db.query(sql, [req.params.id], (err, response) => {
+            if (err) throw err
+            res.json({message: 'Delete success!'})
+        })
+    }
+}
---
+++ controller/controllerProduct.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const util = require('util')
+const mysql = require('mysql')
+const db = require('../db')
+
+module.exports = {
+    get: (req, res) => {
+        let sql = 'SELECT * FROM tbl_product'
+        db.query(sql, (err, response) => {
+            if (err) throw err
+            res.json(response)
+        })
+    },
+    detail: (req, res) => {
+        let sql = 'SELECT * FROM tbl_product WHERE id = ?'
+        db.query(sql, [req.params.id], (err, response) => {
+            if (err) throw err
+            res.json(response[0])
+        })
+    },
+    update: (req, res) => {
+        let data = req.body;
+        let productId = req.params.productId;
+        let sql = 'UPDATE tbl_product SET ? WHERE id = ?'
+        db.query(sql, [data, id], (err, response) => {
+            if (err) throw err
+            res.json({message: 'Update success!'})
+        })
+    },
+    new: (req, res) => {
+        let data = req.body;
+        let sql = 'INSERT INTO tbl_product SET ?'
+        db.query(sql, [data], (err, response) => {
+            if (err) throw err
+            res.json({message: 'Insert success!'})
+        })
+    },
+    delete: (req, res) => {
+        let sql = 'DELETE FROM tbl_product WHERE id = ?'
+        db.query(sql, [req.params.id], (err, response) => {
+            if (err) throw err
+            res.json({message: 'Delete success!'})
+        })
+    }
+}
